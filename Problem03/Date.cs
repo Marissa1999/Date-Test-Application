74947dafd874ccd228be382c0e6a6d4dba6fefc2
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-05 14:01:44 -0500

    Initial Commit.

---
+++ Problem03/Date.cs
@@ -0,0 +1,67 @@
+﻿//ID: 1775227
+//Name: Marissa Goncalves
+//Assignment 1 - Problem 3: DateTest app
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem03
+{
+    class Date
+    {
+
+        //Initialize the instance variables of the Date class
+        private int month;
+        private int day;
+        private int year;
+
+
+        //Main constructor for the Date class that takes all inputted values in the parameter
+        public Date(int month, int day, int year)
+        {
+            Month = month;
+            Day = day;
+            Year = year;
+        }
+
+
+        //Implicit property for the month variable
+        public int Month
+        {
+            get; set;
+        }
+
+
+        //Implicit property for the day variable
+        public int Day
+        {
+            get; set;
+        }
+
+
+        //Implicit property for the year variable
+        public int Year
+        {
+            get; set;
+        }
+
+
+        //Using string interpolation, display the formatted date
+        public void DisplayDate()
+        {
+
+            Console.WriteLine($"----------------------------------");
+            Console.WriteLine($"| Current Date | {Month,2}/{Day,2}/{Year,-10}|");
+            Console.WriteLine($"----------------------------------");
+            Console.WriteLine();
+
+        }
+
+
+
+
+    }
+}
---
+++ Problem03/DateTest.cs
@@ -0,0 +1,49 @@
+﻿//ID: 1775227
+//Name: Marissa Goncalves
+//Assignment 1 - Problem 3: DateTest app
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem03
+{
+    class DateTest
+    {
+        static void Main(string[] args)
+        {
+
+            //Create new objects that take up different values
+            Date one = new Date(9, 23, 1000);
+            Date two = new Date(12, 5, 2019);
+            Date three = new Date(11, 23, 983);
+            Date four = new Date(2, 12, 34);
+            Date five = new Date(10, 3, 5);
+            Date six = new Date(5, 8, 1967);
+            Date seven = new Date(-2, 34, 199);
+            Date eight = new Date(12, -3, -204);
+            Date nine = new Date(9, 3, -13);
+            Date ten = new Date(10, 8, -4);
+
+
+            //Call the method in order to print the date table
+            one.DisplayDate();
+            two.DisplayDate();
+            three.DisplayDate();
+            four.DisplayDate();
+            five.DisplayDate();
+            six.DisplayDate();
+            seven.DisplayDate();
+            eight.DisplayDate();
+            nine.DisplayDate();
+            ten.DisplayDate();
+
+
+
+
+
+        }
+    }
+}
